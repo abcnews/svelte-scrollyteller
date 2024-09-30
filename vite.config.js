3be0780867f14abc1c427a20a408f47a51cdb8df
Ash Kyd <kyd.ashley@abc.net.au>
2024-09-30 13:56:05 +1000

    feature: tweak vite port

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		// Tweak the port so it doesn't conflict with other projects you may be
+		// working on simultaneously
+		port: 5178
+	}
 });
