2dc3210669723d45c56acd5c72199e46c4896d3c
Ash Kyd <kyd.ashley@abc.net.au>
2026-06-18 16:18:58 +1000

    feat: Storybook scrollyteller demo

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/sveltekit';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.stories.svelte",
+    "../src/**/*.stories.ts",
+    "../src/**/*.stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-svelte-csf"
+  ],
+  "framework": "@storybook/sveltekit",
+  "core": {
+    "disableTelemetry": true,
+    "disableWhatsNewNotifications": true
+  },
+  "features": {
+    "sidebarOnboardingChecklist": false
+  }
+};
+export default config;
